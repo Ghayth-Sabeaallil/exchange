528a44212e42ce7154b9b76d3c0aa4fed0738afc
ghayth.sabeaallil <gs222mh@gmail.com>
2024-12-06 11:39:38 +0100

    fix the excahnge and switch

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image, View } from 'react-native';
 export default function HistoryPage() {
   return (
-    <></>
+    <View style={styles.container}>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
 
+  }
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { useState } from 'react';
-import { Button, Pressable, StyleSheet, TextInput, useColorScheme, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, TextInput, useColorScheme, Text, View } from 'react-native';
 import { SelectCountry } from 'react-native-element-dropdown';
 import data from '../../lib/data.json'
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -12,47 +12,54 @@ type PairExchange = {
   target_code: string,
   conversion_rate: number,
   conversion_result: number
+  time_last_update_utc: string
 }
 
 export default function HomeScreen() {
   const colorScheme = useColorScheme();
-  const [from, setFrom] = useState<string>("");
-  const [to, setTo] = useState<string>("");
-  const [fromCountry, setFromCountry] = useState<string>("Select country");
-  const [toCountry, setToCountry] = useState<string>("Select country");
-
-
-  const [reservFrom, setReservFrom] = useState<string>("");
-  const [reservTo, setReservTo] = useState<string>("");
+  const [fromCurrency, setFromCurrency] = useState<string>("");
+  const [toCurrency, setToCurrency] = useState<string>("");
+  const [fromCountry, setFromCountry] = useState<string>("From");
+  const [toCountry, setToCountry] = useState<string>("To");
+  const [fromCurrencySympol, setFromCurrencySympol] = useState<string>("");
+  const [toCurrencySympol, setToCurrencySympol] = useState<string>("");
   const [result, setResult] = useState<PairExchange>();
-  const [amount, setAmout] = useState<string>("1");
+  const [amount, setAmout] = useState<string>("");
+
+  useEffect(() => {
+
+  }, [fromCurrencySympol, toCurrencySympol, result]);
 
   const exchange = async () => {
-    console.log(amount)
-    try {
-      let response;
-      if (amount === "1") {
-        response = await fetch(`https://v6.exchangerate-api.com/v6/b65d0a8f412fdd2f0f965cb2/pair/${from}/${to}`);
-      }
-      else {
-        response = await fetch(`https://v6.exchangerate-api.com/v6/b65d0a8f412fdd2f0f965cb2/pair/${from}/${to}/${amount}`);
-      }
+    if (fromCurrency && toCurrency) {
+      try {
+        let response;
+        if (amount === "") {
+          response = await fetch(`https://v6.exchangerate-api.com/v6/b65d0a8f412fdd2f0f965cb2/pair/${fromCurrency}/${toCurrency}`);
+        }
+        else {
+          response = await fetch(`https://v6.exchangerate-api.com/v6/b65d0a8f412fdd2f0f965cb2/pair/${fromCurrency}/${toCurrency}/${amount}`);
+        }
 
-      const jsonData = await response!.json();
-      setResult(jsonData);
-    } catch (err) {
-      console.error(err)
+        const jsonData = await response!.json();
+        setResult(jsonData);
+      } catch (err) {
+        console.error(err)
+      }
     }
-
-
   }
 
-  /* const change = () => {
-     setReservFrom(from);
-     setReservTo(to);
-     setFrom(reservTo);
-     setTo(reservFrom);
-   }*/
+  const change = () => {
+    if (fromCountry && toCountry) {
+      setFromCountry(toCountry);
+      setToCountry(fromCountry);
+      setFromCurrencySympol(toCurrencySympol);
+      setToCurrencySympol(fromCurrencySympol);
+      setFromCurrency(toCurrency);
+      setToCurrency(fromCurrency);
+      exchange();
+    }
+  }
 
   return (
     <ParallaxScrollView>
@@ -69,16 +76,16 @@ export default function HomeScreen() {
         valueField="alpha2Code"
         labelField="name"
         imageField="flags"
-        placeholder={fromCountry}
+        placeholder={fromCountry + " - " + fromCurrencySympol}
         searchPlaceholder="Search..."
         onChange={e => {
-          setFrom(e.currencies![0].code!);
+          setFromCurrency(e.currencies![0].code);
           setFromCountry(e.name);
-
+          setFromCurrencySympol(e.currencies![0].symbol)
         }}
-        value={from}
+        value={fromCurrency}
       />
-      <Pressable style={styles.switch}>
+      <Pressable style={styles.switch} onPress={change}>
         <IconSymbol size={28} name="switch.2" color={colorScheme === 'dark' ? '#fff' : '#000'} />
       </Pressable>
       <SelectCountry
@@ -94,34 +101,32 @@ export default function HomeScreen() {
         valueField="alpha2Code"
         labelField="name"
         imageField="flags"
-        placeholder={toCountry}
+        placeholder={toCountry + " - " + toCurrencySympol}
         searchPlaceholder="Search..."
         onChange={e => {
-          setTo(e.currencies![0].code!);
+          setToCurrency(e.currencies![0].code!);
           setToCountry(e.name);
+          setToCurrencySympol(e.currencies![0].symbol)
         }}
-        value={to}
+        value={toCurrency}
       />
       <TextInput
         style={styles.input}
         onChangeText={setAmout}
         value={amount}
-        placeholder={`${amount}`}
+        placeholder={"0.0"}
         keyboardType="numeric"
       />
-      <ThemedText>{result && result.base_code}</ThemedText>
-      <ThemedText>{result && result.target_code}</ThemedText>
-      <ThemedText>{amount == "1" && result ? result!.conversion_rate : amount != "1" && result && result!.conversion_result}</ThemedText>
 
+      <Pressable style={styles.btn} onPress={exchange} disabled={false}>
+        <Text style={[styles.text, { fontSize: 16, color: colorScheme === 'dark' ? '#000' : '#fff' }]}>Exchange</Text>
+      </Pressable>
+      <View style={styles.result}>
+        <ThemedText style={styles.text}>{result && result.time_last_update_utc}</ThemedText>
+        <ThemedText type='subtitle' style={styles.text}>{result && `1 ${result.base_code}  =  ${result.conversion_rate} ${result.target_code}`}</ThemedText>
+        <ThemedText type='subtitle' style={styles.text}>{result?.conversion_result && `${amount} ${result.base_code}  =  ${result.conversion_result} ${result.target_code}`}</ThemedText>
 
-
-      <Button
-        onPress={exchange}
-        title="Exchange"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-
+      </View>
     </ParallaxScrollView>
   );
 }
@@ -139,12 +144,13 @@ const styles = StyleSheet.create({
   input: {
     height: 70,
     margin: 12,
-    fontSize: 16,
+    fontSize: 24,
     borderWidth: 1,
     padding: 10,
     backgroundColor: "white",
     color: "black",
-    textAlign: "center"
+    textAlign: "center",
+    borderRadius: 15,
   },
   dropdown: {
     height: 70,
@@ -168,4 +174,22 @@ const styles = StyleSheet.create({
     height: 40,
     fontSize: 16,
   },
+  btn: {
+    backgroundColor: "#601ba1",
+    height: 50,
+    borderRadius: 15,
+    width: "40%",
+    justifyContent: "center",
+    alignContent: "center",
+    alignSelf: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  result: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignSelf: "center",
+  }
 });
